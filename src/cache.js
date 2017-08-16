422a50f59c4d34bab6634713ee7fefe441baefb1
Tom Hastjarjanto <tom@intellicode.nl>
2017-08-16 23:56:52 +0200

    feature/cache: Add cache set feature

--- src/cache.js
+++ src/cache.js
@@ -1,18 +1,23 @@
 const hash = require('object-hash');
 
-function cache(func) {
+function cache(func, options) {
   return (root, args, context) => {
     if (!context.resolverCache) {
       throw new Error('Missing resolverCache property on the Graphql context.');
     }
 
     const key = `${hash(root)}:${hash(args)}`;
+    const executeAndCache = () =>
+      Promise.resolve(func(root, args, context)).then(value =>
+        context.resolverCache.set(key, value, options)
+      );
+
     return (
       context.resolverCache
         .get(key)
-        .then(result => (result ? result : func(root, args, context)))
+        .then(value => (value ? value : executeAndCache()))
         // Error
-        .catch(() => func(root, args, context))
+        .catch(() => executeAndCache())
     );
   };
 }
