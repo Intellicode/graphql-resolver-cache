fcead70329f6d095944961b43146f9686e7bb9c0
Max Nowack <maxnowack@users.noreply.github.com>
2017-10-23 11:09:45 +0200

    Fix cache key and return value on first call

--- src/cache.js
+++ src/cache.js
@@ -1,16 +1,19 @@
 const hash = require('object-hash');
 
+const toSafeObject = obj => JSON.parse(JSON.stringify(obj || {}));
+
 function cache(func, options) {
   return (root, args, context) => {
     if (!context.resolverCache) {
       throw new Error('Missing resolverCache property on the Graphql context.');
     }
 
-    const key = `${hash(root)}:${hash(args)}`;
+    const key = `${hash(func)}:${hash(toSafeObject(root))}:${hash(toSafeObject(args))}`;
     const executeAndCache = () =>
-      Promise.resolve(func(root, args, context)).then(value =>
-        context.resolverCache.set(key, value, options)
-      );
+      Promise.resolve(func(root, args, context)).then((value) => {
+        context.resolverCache.set(key, value, options);
+        return value;
+      });
 
     return (
       context.resolverCache
